53efcbc292079952cae18edb14ad17531f2e78a4
Zakaria ALLA <z.alla@mundiapolis.ma>
2025-11-13 00:20:11 +0100

    fix routes*

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         ]
       : []),
   ],
-  base: '/colis-voyageur/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
