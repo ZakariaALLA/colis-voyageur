ed9bef822186fa3cfab46d4aa2d1b52146e5e820
Zakaria ALLA <z.alla@mundiapolis.ma>
2025-11-15 19:52:36 +0100

    update base repo

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         ]
       : []),
   ],
-  base: '/',
+  base: '/colis-voyageur/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
